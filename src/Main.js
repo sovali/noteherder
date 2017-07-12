1bf4dad6731cc09e3cdc5f51d1decd413f7cc5d4
Selin Ovali <sovali@purdue.edu>
2017-07-12 09:42:23 -0400

    Formatted

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import {Route,Switch} from 'react-router-dom'
 import './Main.css'
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
 const Main = (props) => {
+  const formProps = {
+            notes: props.notes,
+            currentNoteId: props.currentNoteId,
+            saveNote: props.saveNote,
+            removeNote: props.removeNote
+  }
   return (
     <div className="Main">
       <Sidebar
@@ -16,12 +22,22 @@ const Main = (props) => {
         notes={props.notes}
         setCurrentNoteId={props.setCurrentNoteId}
       />
-      <NoteForm
-        notes={props.notes}
-        currentNoteId={props.currentNoteId}
-        saveNote={props.saveNote}
-        removeNote={props.removeNote}
-      />
+
+      <Switch>
+          <Route path="/notes/:id" render={(navProps) => (
+             <NoteForm
+                {...formProps}
+                {...navProps}
+          />
+          )} />
+
+          <Route render={(navProps) => (
+             <NoteForm
+                {...formProps}
+                {...navProps}
+          />
+          )} />
+      </Switch>
     </div>
   )
 }
